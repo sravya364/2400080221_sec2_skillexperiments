bc1b28cab8f28ba9f60f92485c93893426ee476f
Sravya Muthyala <2400080221@kluniversity.in>
2026-03-06 00:39:11 +0530

    Add files via upload

---
+++ spring-di-student/src/main/java/com/klu/spring_di_student/Student.java
@@ -0,0 +1,34 @@
+package com.klu.spring_di_student;
+
+public class Student {
+
+    private int studentId;
+    private String name;
+    private String course;
+    private int year;
+
+    // Constructor Injection
+    public Student(int studentId, String name, String course, int year) {
+        this.studentId = studentId;
+        this.name = name;
+        this.course = course;
+        this.year = year;
+    }
+
+    // Setter Injection
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // Display method
+    public void display() {
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Name: " + name);
+        System.out.println("Course: " + course);
+        System.out.println("Year: " + year);
+    }
+}
